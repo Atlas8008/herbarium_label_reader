626ade692ea42712644a1d421d024fd58711b9cc
koerschens <matthias.koerschens@uni-jena.de>
2025-07-30 17:18:00 +0200

    Improvement of eval all script

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-for folder in $1/*; do
-    if [ -d "$folder" ]; then
-        echo "Processing folder: $folder"
-        echo $(basename "$folder") >> "$1/evaluation_results.txt"
-        python evaluate.py --extracted_csv "$folder/extracted_data.csv" --ground_truth_csv ~/datasets/herbarium_scans_senckenberg/2024_14\ P\ Anhang_Belegdaten.csv >> "$1/evaluation_results.txt"
-        echo >> "$1/evaluation_results.txt"
-    else
-        echo "Skipping non-directory: $folder"
-    fi
-done
+
+python evaluate.py \
+    --extracted_csv "$1"/*image_list=data.handwritten.txt*/extracted_data.csv \
+    --ground_truth_csv ~/datasets/herbarium_scans_senckenberg/handwritten/label_data.csv \
+    --output_csv "$1/evaluation_results_handwritten.csv"
+
+python evaluate.py \
+    --extracted_csv "$1"/*image_list=data.printed.txt*/extracted_data.csv \
+    --ground_truth_csv ~/datasets/herbarium_scans_senckenberg/printed/label_data.csv \
+    --output_csv "$1/evaluation_results_printed.csv"
+
+
+# for folder in $1/*; do
+#     if [ -d "$folder" ]; then
+#         echo "Processing folder: $folder"
+#         echo $(basename "$folder") >> "$1/evaluation_results.txt"
+#         python evaluate.py --extracted_csv "$folder/extracted_data.csv" --ground_truth_csv ~/datasets/herbarium_scans_senckenberg/2024_14\ P\ Anhang_Belegdaten.csv >> "$1/evaluation_results.txt"
+#         echo >> "$1/evaluation_results.txt"
+#     else
+#         echo "Skipping non-directory: $folder"
+#     fi
+# done
